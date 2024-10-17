5027abc14d44935b20bbee670ee86943a0e5ed4b
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-17 02:37:34 +0100

    tested all routes add they are working as expected

--- api/Blog/blogApi/views.py
+++ api/Blog/blogApi/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response  import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
-
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 
 
@@ -13,7 +13,7 @@ def createBlog(request):
     if request.method == 'POST':
         serializer = BlogPostSerializer(data=request.data)
 
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -33,6 +33,7 @@ def getBlog(request):
 
 
 # Blog update view
+@api_view(['PUT'])
 def updateBlog(request, pk):
     blogpost = get_object_or_404(BlogPost, pk=pk)
 
@@ -72,23 +73,23 @@ def deleteBlog(request, pk):
 
 
 
-# Class based view using DRF generics
-class BlogPostListCreate(generics.ListCreateAPIView):
-    queryset = BlogPost.objects.all() # getting all of the blogpost models that exist
-    serializer_class = BlogPostSerializer
+# # Class based view using DRF generics
+# class BlogPostListCreate(generics.ListCreateAPIView):
+#     queryset = BlogPost.objects.all() # getting all of the blogpost models that exist
+#     serializer_class = BlogPostSerializer
 
 
-# using custom functions to overide. That's to delete all of the different blog posts that exist
-def delete(self, request, *args, **kwargs):
-    BlogPost.objects.all()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+# # using custom functions to overide. That's to delete all of the different blog posts that exist
+# def delete(self, request, *args, **kwargs):
+#     BlogPost.objects.all()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
 
-# creating a new view which can be used to update and delete blog post by id
+# # creating a new view which can be used to update and delete blog post by id
 
-class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer
-    lookup_field = 'pk' # which is the primary key that's the id
+# class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = BlogPost.objects.all()
+#     serializer_class = BlogPostSerializer
+#     lookup_field = 'pk' # which is the primary key that's the id
 
 
 
