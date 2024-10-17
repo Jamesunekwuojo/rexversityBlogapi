42896f05595bf6aee0dc8aba7571d03dd469ffde
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-17 01:32:34 +0100

    blogApi app url updated to match the custom views created

--- api/Blog/blogApi/urls.py
+++ api/Blog/blogApi/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views # dot means the current  directory
+from . import views
 
 urlpatterns = [
-    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
-    path("blogposts/<int:pk>", views.BlogPostRetrieveUpdateDestroy.as_view(), name='update')
+    path("blogposts/", views.getBlog, name="get-blog"),  # To Get all blogs
+    path("blogposts/create/", views.createBlog, name="create-blog"),  # To Create a new blog post
+    path("blogposts/<int:pk>/", views.updateBlog, name="update-blog"),  #To  Update a blog post by ID
+    path("blogposts/<int:pk>/delete/", views.deleteBlog, name="delete-blog"),  # Yo Delete a blog post by ID
 ]
-
-
